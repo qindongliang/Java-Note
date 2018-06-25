1bdd58863aa9cca4573d1a3f29443e5cb7915098
qindongliang <qindongliang@xueleyun.com>
2018-06-25 16:03:51 +0800

    composite action like counter++ or counter-- exist problem if use volatile keyword . it is no atomicity

---
+++ src/main/java/note/concurrent/VolatileNoAtomic.java
@@ -0,0 +1,94 @@
+package note.concurrent;
+
+/**
+ * Windows 10 + JDK 8
+ */
+public class VolatileNoAtomic {
+
+    // volatile variable
+    static volatile int c;
+
+    public static void main(String[] args) throws Exception{
+
+
+            // volatile only guarantee visibility no atomicity
+            problemDemo();
+
+
+            // how to fix the prolem ?
+
+           //  1 : use synchronized methon
+
+          //   2 : use  AtomicInteger
+
+
+
+
+
+
+    }
+
+    public static void useSynchronized(){
+
+    }
+
+
+
+
+    public static synchronized void increment(){
+        c++;
+    }
+
+
+
+    public static void problemDemo() throws  Exception{
+
+
+        for (int i = 0; i < 10; i++) {
+
+            //  reset 0
+            c=0;
+
+            // thread 1
+
+            Thread t1=new Thread(()->{
+
+                for (int j = 0; j < 1000; j++) {
+                    c++;
+                }
+
+            });
+
+            // thread 2
+
+            Thread t2=new Thread(()->{
+
+                for (int j = 0; j < 1000; j++) {
+                    c++;
+                }
+
+            });
+
+            t1.start();
+            t2.start();
+
+            t1.join();
+            t2.join();
+
+
+            System.out.println("count value: "+c);
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
