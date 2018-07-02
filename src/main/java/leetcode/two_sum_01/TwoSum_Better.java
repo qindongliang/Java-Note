4cf21c9975580b7be2644b9d430e620bcd097cde
Qin Dong Liang <182100507492@126.com>
2018-07-02 23:44:38 +0800

    leetcode算法01的模块两个数相加，性能较好的写法

---
+++ src/main/java/leetcode/two_sum_01/TwoSum_Better.java
@@ -0,0 +1,39 @@
+package leetcode.two_sum_01;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Created by qindongliang on 2018/7/2.
+ */
+public class TwoSum_Better {
+
+    public static int[] twoSum(int nums[],int target){
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        for (int i = 0; i <nums.length ; i++) {
+            int temp=target-nums[i];
+            Integer temp_index=map.get(temp);
+            if(temp_index!=null){
+                return new int[]{i,temp_index};
+            }
+
+            map.put(nums[i],i);
+
+
+        }
+        return null;
+    }
+
+
+
+    @Test
+    public  void test(){
+        int nums[]={2,7,11,5};
+        int target=9;
+        System.out.println(Arrays.toString(twoSum(nums,target)));
+    }
+}
