a4110e6c94e75c6c7f82cf5c8b9eea120b487b3a
Qin Dong Liang <182100507492@126.com>
2018-07-05 23:08:10 +0800

    简单的一个饿汉的单例模式

--- src/main/java/design_pattern/singleton/SimpleSingleton.java
+++ src/main/java/design_pattern/singleton/SimpleSingleton.java
@@ -4,7 +4,7 @@ package design_pattern.singleton;
  * Created by qindongliang on 2018/7/5.
  */
 public class SimpleSingleton {
-    private static SimpleSingleton ourInstance = new SimpleSingleton();
+    private static  SimpleSingleton ourInstance = new SimpleSingleton();
 
     public static SimpleSingleton getInstance() {
         return ourInstance;
