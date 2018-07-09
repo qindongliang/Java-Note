f7edbe6f8419dc622634802ec1cfcd9266358a9d
Qin Dong Liang <182100507492@126.com>
2018-07-09 22:12:57 +0800

    关于变量可见性问题2

---
+++ src/main/java/concurrent/VariableVisilityProblem2.java
@@ -0,0 +1,40 @@
+package concurrent;
+
+/**
+ * Created by qindongliang on 2018/7/9.
+ */
+public class VariableVisilityProblem2 {
+
+    private  static boolean flag=true; // main thread will call flag=false
+
+    private final static Object lock=new Object(); // lock condition
+
+    public static void thread1(){
+
+        while (flag){
+
+            synchronized (lock){
+                // some work
+            }
+
+        }
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        Thread t1=new Thread(()->{
+            thread1();
+        });
+        t1.start();
+        Thread.sleep(1000);
+        flag=false;
+
+        // The program can stop normally
+
+    }
+
+
+
+}
