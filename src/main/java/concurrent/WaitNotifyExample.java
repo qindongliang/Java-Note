743d3ddd7efd703e8e0da01d22f61e0f8c948fb0
qindongliang <qindongliang@xueleyun.com>
2018-07-12 15:10:14 +0800

    wait和notify例子1

---
+++ src/main/java/concurrent/WaitNotifyExample.java
@@ -0,0 +1,77 @@
+package concurrent;
+
+/**
+ * Created by Administrator on 2018/6/27.
+ */
+public class WaitNotifyExample {
+
+    private static String message;
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        Object lock=new Object();
+
+
+        Thread t1=new Thread(()->{
+
+            synchronized (lock){
+                System.out.println("message: "+message);
+
+
+                while (message==null){
+                    try {
+                        System.out.println("开始阻塞...1");
+                        lock.wait();
+                        System.out.println("开始阻塞...2");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                System.out.println(message);
+            }
+
+
+        });
+
+//
+//        Thread t2=new Thread(()->{
+//
+//            synchronized (lock){
+//                message="a message from thread ! ";
+//                try {
+//                    Thread.sleep(5000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                lock.notify();
+//
+//            }
+//
+//
+//        });
+
+
+        t1.start();
+
+
+        Thread.sleep(3000);
+
+        synchronized (lock) {
+            lock.notify();
+
+        }
+//        t1.interrupt();
+
+//        System.exit(0);
+
+//        t2.start();
+
+
+
+
+    }
+
+
+}
