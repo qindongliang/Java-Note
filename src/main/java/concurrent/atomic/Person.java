ca335ccb9439995743227a3cc84f20390b3263f3
qindongliang <qindongliang@xueleyun.com>
2018-07-20 15:43:43 +0800

    添加po为了测试

---
+++ src/main/java/concurrent/atomic/Person.java
@@ -0,0 +1,13 @@
+package concurrent.atomic;
+
+/**
+ * Created by Administrator on 2018/7/20.
+ */
+public class Person {
+
+    private int age;
+
+    public Person(int age) {
+        this.age = age;
+    }
+}
