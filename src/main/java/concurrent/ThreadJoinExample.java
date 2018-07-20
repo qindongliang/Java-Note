1d68982eea674e785564ec59cfc22cd21a7328d0
qindongliang <qindongliang@xueleyun.com>
2018-07-20 15:00:12 +0800

    join例子更新

--- src/main/java/concurrent/ThreadJoinExample.java
+++ src/main/java/concurrent/ThreadJoinExample.java
@@ -11,23 +11,49 @@ public class ThreadJoinExample {
         // join方法，会让主线程等待当前线程执行完，再执行本身
         // join（time）可以让主线程阻塞指定的时间
 
-       Thread tx=  new Thread(()->{
+       Thread t1=  new Thread(()->{
 
            System.out.println(Thread.currentThread().getName());
            try {
-               Thread.sleep(5000);
+               Thread.sleep(3000);
            } catch (InterruptedException e) {
                e.printStackTrace();
            }
 
-           System.out.println("5 second ....");
+           System.out.println("3 second ....");
 
        });
 
-        tx.start();
-        tx.join(1000);
+        Thread t2=  new Thread(()->{
 
-        System.out.println("txx");
+            System.out.println(Thread.currentThread().getName());
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("2 second ....");
+
+        });
+
+        //下面是串行执行
+        t1.start();
+        t1.join();
+        //必须t1执行完才会执行下面的t2线程
+        t2.start();
+        t2.join();
+
+
+        //下面是并行执行
+        //t1.start();
+        //t2.start();
+        //t1.join();
+        //t2.join();
+
+
+
+        System.out.println("main thread");
 
 
 
