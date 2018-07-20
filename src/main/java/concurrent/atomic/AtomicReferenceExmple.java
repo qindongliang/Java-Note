8246e8ff3add24f2fb2da3ac88e411a566febf29
qindongliang <qindongliang@xueleyun.com>
2018-07-20 15:42:19 +0800

    添加atomic引用类的例子

---
+++ src/main/java/concurrent/atomic/AtomicReferenceExmple.java
@@ -0,0 +1,14 @@
+package concurrent.atomic;
+
+/**
+ * Created by Administrator on 2018/7/20.
+ */
+public class AtomicReferenceExmple {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
