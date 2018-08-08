24ce06a5caece07184fa84f6ce609a936d8d3114
qindongliang <qindongliang@xueleyun.com>
2018-08-08 20:11:49 +0800

    LockSupport互斥队列的例子

---
+++ src/main/java/concurrent/lock/LockSupportFIFOMutexExample.java
@@ -0,0 +1,49 @@
+package concurrent.lock;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * Created by Administrator on 2018/8/7.
+ */
+public class LockSupportFIFOMutexExample {
+
+    private final AtomicBoolean locked = new AtomicBoolean(false);
+    private final Queue<Thread> waiters = new ConcurrentLinkedQueue<Thread>();
+
+    public void lock() {
+        boolean wasInterrupted = false;
+        Thread current = Thread.currentThread();
+        //先把自己加到队列里面
+        waiters.add(current);
+
+        // Block while not first in queue or cannot acquire lock
+        //peek取出头元素但不移除，判断头元素是否等当前线程 ，如果不是当前元素，直接进入循环
+        //如果头不是当前线程，那么判断
+        while (waiters.peek() != current || !locked.compareAndSet(false, true)) {
+            LockSupport.park(this);
+            if (Thread.interrupted()) // ignore interrupts while waiting
+                wasInterrupted = true;
+        }
+
+        waiters.remove();
+        if (wasInterrupted)          // reassert interrupt status on exit
+            current.interrupt();
+    }
+
+    public void unlock() {
+        locked.set(false);
+        LockSupport.unpark(waiters.peek());
+    }
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+}
