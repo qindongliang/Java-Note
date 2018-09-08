f1a146498db22ed880b80164c0bf7c2c2d327e51
Qin Dong Liang <182100507492@126.com>
2018-09-08 23:37:11 +0800

    算法的复杂度

---
+++ src/main/java/sort_algorithm/TimeComplexDemo.java
@@ -0,0 +1,19 @@
+package sort_algorithm;
+
+/**
+ * Created by qindongliang on 2018/9/8.
+ */
+public class TimeComplexDemo {
+
+    public void constantTime(int a,int b){
+        int sum=a+b;
+        System.out.println(sum);
+    }
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
