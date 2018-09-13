20d63072561b1d26e2fa528356dd5920c91c5ea7
qindongliang <qindongliang@xueleyun.com>
2018-09-14 02:09:23 +0800

    添加测试拒绝策略的例子

--- src/main/java/concurrent/threadpool/ThreadPoolExecutorTest.java
+++ src/main/java/concurrent/threadpool/ThreadPoolExecutorTest.java
@@ -7,7 +7,10 @@ public class ThreadPoolExecutorTest {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
 
-        ThreadPoolExecutor pool=(ThreadPoolExecutor)Executors.newFixedThreadPool(2);
+//        ThreadPoolExecutor pool=(ThreadPoolExecutor)Executors.newFixedThreadPool(1);
+        //测试拒绝策略，
+        ThreadPoolExecutor pool=new ThreadPoolExecutor(1,1,
+                60,TimeUnit.SECONDS,new LinkedBlockingQueue<>(1));
 
 //        pool.allowCoreThreadTimeOut(true);
 
@@ -21,15 +24,17 @@ public class ThreadPoolExecutorTest {
 //        });
 
 
-//        FutureTask<String> f1=(FutureTask) pool.submit(new Callable<String>() {
-//
-//            @Override
-//            public String call() {
-//
-//                System.out.println(Thread.currentThread().getName()+"  println ok2   ");
-//                return "ok2";
-//            }
-//        });
+        FutureTask<String> f1=(FutureTask) pool.submit(new Callable<String>() {
+
+            @Override
+            public String call() throws InterruptedException {
+
+                System.out.println(Thread.currentThread().getName()+"  println ok1   ");
+                TimeUnit.MINUTES.sleep(1253);
+                System.out.println(Thread.currentThread().getName()+"  println ok2   ");
+                return "ok2";
+            }
+        });
 
 //        pool.submit(new Runnable() {
 //            @Override
@@ -46,7 +51,15 @@ public class ThreadPoolExecutorTest {
             }
         },456);
 
-        System.out.println(get.get());
+
+        FutureTask<Integer> get1=(FutureTask) pool.submit(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("ok111....");
+            }
+        },456111);
+
+        System.out.println(get1.get());
         pool.shutdown();
 
 
