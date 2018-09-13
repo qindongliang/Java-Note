1e900f95842dd7a38c6823ac2791b038b51a4538
qindongliang <qindongliang@xueleyun.com>
2018-09-14 01:12:44 +0800

    线程池测试例子

--- src/main/java/concurrent/threadpool/ThreadPoolExecutorTest.java
+++ src/main/java/concurrent/threadpool/ThreadPoolExecutorTest.java
@@ -1,27 +1,53 @@
 package concurrent.threadpool;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
 public class ThreadPoolExecutorTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
 
-        ThreadPoolExecutor pool=(ThreadPoolExecutor)Executors.newFixedThreadPool(4);
+        ThreadPoolExecutor pool=(ThreadPoolExecutor)Executors.newFixedThreadPool(2);
 
-        pool.allowCoreThreadTimeOut(true);
+//        pool.allowCoreThreadTimeOut(true);
 
 
-        pool.submit(new Runnable() {
+//     FutureTask result=(FutureTask)pool.submit(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                System.out.println("go......");
+//            }
+//        });
+
+
+//        FutureTask<String> f1=(FutureTask) pool.submit(new Callable<String>() {
+//
+//            @Override
+//            public String call() {
+//
+//                System.out.println(Thread.currentThread().getName()+"  println ok2   ");
+//                return "ok2";
+//            }
+//        });
+
+//        pool.submit(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println(Thread.currentThread().getName()+" 运行了......");
+//            }
+//        });
+
+
+       FutureTask<Integer> get=(FutureTask) pool.submit(new Runnable() {
             @Override
             public void run() {
-
-                System.out.println("go......");
+                System.out.println("ok....");
             }
-        });
+        },456);
 
+        System.out.println(get.get());
+        pool.shutdown();
 
 
 
