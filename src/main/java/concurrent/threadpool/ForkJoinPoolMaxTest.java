76b363deace30a174dd5bb2c55e5ed08a5388ca0
Qin Dong Liang <182100507492@126.com>
2018-09-15 13:27:23 +0800

    注释改造

--- src/main/java/concurrent/threadpool/ForkJoinPoolMaxTest.java
+++ src/main/java/concurrent/threadpool/ForkJoinPoolMaxTest.java
@@ -37,15 +37,10 @@ public class ForkJoinPoolMaxTest {
 
 //        ForkJoinPool forkJoinPool=ForkJoinPool.commonPool();
 
-        int array[]=  getIntArray(10);
-//        long start=System.currentTimeMillis();
-//        System.out.println( getMaxValue(array));
-//        long end=System.currentTimeMillis();
-//        System.out.println("串行耗时："+(end-start)+" ms ");
-
+        int array[]=  getIntArray(10);//构建数组
         ForkJoinPool pool=new ForkJoinPool();//实例化pool，构造函数可传入并行度
        long start=System.currentTimeMillis();
-      FindMaxTask fmt=  new FindMaxTask(array,0,array.length);
+       FindMaxTask fmt=  new FindMaxTask(array,0,array.length);
         //Integer max=pool.invoke(fmt);//第一种调用方法
        Integer max= fmt.invoke();//第二种调用方法，使用默认的通用线程池
         System.out.println(max);
@@ -69,10 +64,9 @@ public class ForkJoinPoolMaxTest {
 
         @Override
         protected Integer compute() {
-
             //如果太小就不分割，直接计算
             if(end-start<=2){
-                int max=-99;
+                int max=-1;
                 for (int i = start; i <end ; i++) {
                     max=Integer.max(max,array[i]);
                 }
@@ -80,18 +74,13 @@ public class ForkJoinPoolMaxTest {
             }else{
                 //否则就分成两个数组，分别进行处理
                 int mid=(end-start)/2+start;
-
                 FindMaxTask left=new FindMaxTask(array,start,mid);
                 FindMaxTask right=new FindMaxTask(array,mid,end);
 
                  invokeAll(right,left);
-
                 int leftRes=left.getRawResult();
                 int rightRes=right.getRawResult();
-
-
                 return Integer.max(leftRes,rightRes);//第一种方法
-//                return Integer.max(left.join(),right.join());
 
             }
         }
