d7897cbe8457e40b44e717e310bd4b8144a14ee1
Qin Dong Liang <182100507492@126.com>
2018-09-23 20:40:51 +0800

    测试类修改

---
+++ src/main/java/basic/Test.java
@@ -0,0 +1,20 @@
+package basic;
+
+/**
+ * Created by qindongliang on 2018/9/23.
+ */
+public class Test {
+
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+
+        //默认是类初始化，可以选择关闭
+        Class c= Class.forName("basic.ReturnTest");
+
+
+        //关闭类的初始化
+        Class c1= Class.forName("basic.ReturnTest",false,ReturnTest.class.getClassLoader());
+        c1.newInstance();//实例初始化
+    }
+
+
+}
