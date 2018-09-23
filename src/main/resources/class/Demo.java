89067626f9c0cd5af0040bb615517b3f19019140
Qin Dong Liang <182100507492@126.com>
2018-09-23 23:27:22 +0800

    修改测试的加载类

--- src/main/resources/class/Demo.java
+++ src/main/resources/class/Demo.java
@@ -1,5 +1,10 @@
 public class Demo{
 
+    public Demo(){
+        System.out.println(" Demo构造函数被初始化了....... ");
+    }
+
+
    public void say(){
         System.out.println("Say Hello .........");
     }
