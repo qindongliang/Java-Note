7f408bdad30753398ec37e19ed4212196dcff448
Qin Dong Liang <182100507492@126.com>
2018-09-23 19:36:55 +0800

    测试类

---
+++ src/main/java/class_loader/TestInitOrder.java
@@ -0,0 +1,17 @@
+package class_loader;
+
+/**
+ * Created by qindongliang on 2018/9/23.
+ */
+public class TestInitOrder {
+
+    public static void main(String[] args) {
+
+
+        
+        Too too=new Too();
+
+
+
+    }
+}
