95f5b5c06233f4f27653582a8d0e39d15bc98289
Qin Dong Liang <182100507492@126.com>
2018-09-24 16:56:15 +0800

    提交相关的测试信息

--- src/main/java/basic/Test.java
+++ src/main/java/basic/Test.java
@@ -8,12 +8,12 @@ public class Test {
     public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
 
         //默认是类初始化，可以选择关闭
-        Class c= Class.forName("basic.ReturnTest");
+//        Class c= Class.forName("basic.ReturnTest");
 
 
         //关闭类的初始化
         Class c1= Class.forName("basic.ReturnTest",false,ReturnTest.class.getClassLoader());
-        c1.newInstance();//实例初始化
+//        c1.newInstance();//实例初始化
     }
 
 
