ffff61d99041f14b072dd362aea590e6f1fcaf7e
qindongliang <qindongliang@xueleyun.com>
2018-09-25 19:16:30 +0800

    测试类加载的异常

---
+++ src/main/java/class_loader/exception/ExceptionTest.java
@@ -0,0 +1,13 @@
+package class_loader.exception;
+
+public class ExceptionTest {
+
+    public static void main(String[] args)throws Exception {
+
+
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+
+
+
+    }
+}
