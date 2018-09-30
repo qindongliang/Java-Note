d4d83bd3ab5de92d7a8518e26c41512050ff3af3
qindongliang <qindongliang@xueleyun.com>
2018-09-30 13:33:13 +0800

    java反射的例子修改

--- src/main/java/reflection/Person.java
+++ src/main/java/reflection/Person.java
@@ -6,9 +6,26 @@ public class Person {
 
     private int age;
 
-    public String address;
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+        System.out.println("name： "+name+"   age: "+age);
+    }
+
+    public String address="北京";
 
     String id;
+
+
+    public void speak(String place,String status,int day){
+
+        System.out.println(place+"   "+status+"    "+day);
+
+    }
 
 
 }
--- src/main/java/reflection/ReflectionTest.java
+++ src/main/java/reflection/ReflectionTest.java
@@ -1,20 +1,51 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class ReflectionTest {
 
-    public static void main(String[] args) {
+    //非反射方式，获取类的内部信息
+    public void test1(){
 
-        Person person=new Person();
+//        Person person=new Person();
+//
+//        Field[] fields = person.getClass().getDeclaredFields();
+//        //Field[] fields = person.getClass().getFields();// 仅仅是public修饰符的
+//
+//        for(Field f:fields){
+//            System.out.println(f);
+//        }
 
-        Field[] fields = person.getClass().getDeclaredFields();
-        //Field[] fields = person.getClass().getFields();// 仅仅是public修饰符的
+
+    }
 
 
-        for(Field f:fields){
-            System.out.println(f);
-        }
+    public static void main(String[] args)throws Throwable {
+
+
+       Class c=  Class.forName("reflection.Person");
+
+       Person p1= (Person) c.newInstance();//默认的无参数构造函数
+        //访问带参数的方法
+       Method m= c.getDeclaredMethod("speak",new Class[]{String.class,String.class,int.class});
+       m.invoke(p1,"洛阳","不错",7);
+
+       //调用有参构造函数
+       System.out.println("========================================");
+       Constructor c2=c.getConstructor(new Class[]{String.class,int.class});
+       Person p2= (Person)c2.newInstance("你好",45);
+
+
+       Person p3=new Person();
+       Person p4=new Person();
+
+        System.out.println(p3.getClass()==p4.getClass());
+
+
+
+
 
 
 
