2baed69b0d864cd545d9a80c0b9aec1556f0b04e
qindongliang <18210507492@126.com>
2018-10-13 17:08:44 +0800

    阶乘的递归解决例子

---
+++ src/main/java/algorithm/recursion/FactorialRecursion.java
@@ -0,0 +1,33 @@
+package algorithm.recursion;
+
+/**
+ *
+ * 使用递归算法实现阶乘的例子
+ *
+ * Created by qindongliang on 2018/10/13.
+ */
+public class FactorialRecursion {
+
+
+    public static int factrial(int n){
+        if(n<1){
+            System.out.println("拆解问题完毕，开始分而治之");
+            return 1;
+        }
+        System.out.println("f("+n+")="+n+" * f("+(n-1)+")");
+        int z= n*factrial(n-1);
+
+        System.out.println("f("+n+")="+z);
+
+        return  z;
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println(factrial(5));
+
+    }
+}
