5f1843e6417be3ca3bbe85b453838a0272dc5e90
Qin Dong Liang <18210507492@126.com>
2018-10-13 21:48:46 +0800

    基本的递归调用例子

--- src/main/java/algorithm/recursion/RecursionDemo.java
+++ src/main/java/algorithm/recursion/RecursionDemo.java
@@ -5,28 +5,44 @@ package algorithm.recursion;
  */
 public class RecursionDemo {
 
-    static int count=0;
+    static int count1=0;
+    static int count2=0;
 
 
     public static void test1(){
-        if(count==5){
-            System.out.println("=============拆解完毕，开始分治==================");
+        if(count1==5){
+            System.out.println("=============递归1拆解完毕，开始分治==================");
             return;
         }
-        count++;
-        System.out.println("递归调用，当前栈深度： "+count);
+        count1++;
+        System.out.println("递归方法1调用，当前栈深度： "+count1);
         test1();
-        System.out.println("递归返回，当前栈深度： "+count--);
-
-
-
+        System.out.println("递归方法1返回，当前栈深度： "+count1--);
     }
+
+
+    public static void test2(){
+
+        if(count2==5){
+            System.out.println("=============递归2拆解完毕，开始分治==================");
+            return;
+        }
+        count2++;
+        System.out.println("递归方法2调用，当前栈深度： "+count2);
+        test2();
+        System.out.println("***********递归方法2返回，当前栈深度： "+count2--+"**************");
+        test1();
+//        System.out.println("递归返回，当前栈深度： "+count2--);
+    }
+
 
 
     public static void main(String[] args) {
 
 
-        test1();
+//        test1();
+
+        test2();
 
     }
 
