4c88bdcde1cf37038193e283d625ef7134ef1459
qindongliang <qindongliang@xueleyun.com>
2018-10-18 20:01:06 +0800

    代码位置调整

--- src/main/java/singleton/create/SimpleSingleton.java
+++ src/main/java/singleton/create/SimpleSingleton.java
@@ -4,12 +4,13 @@ package singleton.create;
  * Created by qindongliang on 2018/7/5.
  */
 public class SimpleSingleton {
+
     private final static  SimpleSingleton ourInstance = new SimpleSingleton();
+
+    private SimpleSingleton() {
+    }
 
     public static SimpleSingleton getInstance() {
         return ourInstance;
-    }
-
-    private SimpleSingleton() {
     }
 }
