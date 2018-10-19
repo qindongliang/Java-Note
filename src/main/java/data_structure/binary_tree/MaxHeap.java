4a045ec79d0576be523808fbce42c82833bf9f2d
qindongliang <qindongliang@xueleyun.com>
2018-10-19 19:09:08 +0800

    添加最大堆和最小堆例子

---
+++ src/main/java/data_structure/binary_tree/MaxHeap.java
@@ -0,0 +1,190 @@
+package data_structure.binary_tree;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/****
+ * 构建堆的特点：
+ * （1）符合完全二叉树的定义，最底的一层可以缺少节点，并新增的集中在左边，此外其他的层必须都是满的
+ * （2）任意父节点的值，是左右子树的最大值,此外左右孩子的大小关系不做任何要求
+ * （3）可以采用数组实现
+ * （4）已知父节点i的位置，那么 左孩子=2*i，右孩子=2*i+1
+ * （5）已知左节点/右节点的位置i，那么父节点位置=i/2 注意结果是向下取整
+ */
+public class MaxHeap   {
+
+     List<Integer> maxHeap;
+
+    public MaxHeap() {
+        this.maxHeap = new ArrayList<Integer>();
+        maxHeap.add(0,null);
+    }
+
+    /***
+     * 维持一个容量固定的topN队列
+     * @param n
+     */
+    public void topN(int n){
+        List<Integer> topN=new ArrayList<>();
+        topN.add(0,null);
+        n=Math.min(3,maxHeap.size()-1);
+        for(int i=1;i<=n;i++){
+            topN.add(getMaxValue());
+            delete(getMaxValue());
+        }
+        this.maxHeap=topN;
+
+    }
+
+    public void insert(Integer value){
+        //新插入的元素，放在数组最后，保持完全二叉树的特性
+        maxHeap.add(value);
+
+        //获取最后一个元素在数组中的索引位置，从1开始
+        int index=maxHeap.size()-1;
+        //父节点的位置
+        int parentIndex=index/2;
+
+
+
+        //在数组范围内，比较这个插入值和其父节点的大小关系，大于父节点则用父节点替换当前值
+        //index位置上升
+        while (index>1){
+
+            int parent=maxHeap.get(parentIndex);//获取父节点的值
+
+            if(compare(value,parent)<=0){
+                break;
+            }else{
+                //如果当前的值大于父节点的值，就把父节点的值，赋值给当前节点
+                maxHeap.set(index,parent);
+
+                index=parentIndex;//改变index的位置
+                parentIndex=index/2;//改变父节点的位置
+
+            }
+
+
+        }
+        //把新加入的值，放到其合适的位置
+        maxHeap.set(index,value);
+    }
+
+
+    public Integer getMaxValue(){
+        if (maxHeap.size()==1){
+            System.out.println("当前是空堆");
+             return  null;
+        }else{
+            return maxHeap.get(1);
+        }
+    }
+
+    /***
+     *
+     * @param a
+     * @param b
+     * @return 负数代表小于，0代表等于，正数代表大于
+     */
+    private int compare(Integer a,Integer b){
+        return a.compareTo(b);
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        //维护一个topN的最大值
+        MaxHeap maxHeap=new MaxHeap();
+        maxHeap.insert(1);
+        maxHeap.insert(92);
+        maxHeap.insert(11);
+        maxHeap.insert(58);
+        maxHeap.insert(41);
+        maxHeap.insert(2);
+        maxHeap.insert(2);
+        maxHeap.insert(120);
+
+        maxHeap.topN(4);
+        System.out.println(maxHeap.maxHeap);
+//        System.out.println(minHeap.getMaxValue());
+//
+//        System.out.println("================最大堆排序==================");
+//
+//        minHeap.heapSort();
+
+
+
+    }
+
+    public  void  heapSort(){
+        int []sorted=new int[maxHeap.size()-1];
+        int count=0;
+        for (int k = 1; k <=sorted.length; k++) {
+            int maxValue=getMaxValue();
+            sorted[count++]=maxValue;
+            delete(maxValue);
+//            System.out.println(minHeap);
+        }
+        System.out.println(Arrays.toString(sorted));
+    }
+
+
+    /***
+     * 堆的任意值的删除
+     * @param value
+     * @return
+     */
+    public boolean delete(Integer value){
+
+       if(maxHeap.isEmpty()){
+           return false;
+       }
+
+       int index =maxHeap.indexOf(value);
+       if(index==-1){return  false;}
+
+       int lastIndex=maxHeap.size()-1;
+
+       //得到最后一个元素的值
+       Integer temp=maxHeap.get(lastIndex);
+       maxHeap.set(index,temp);//用最后一个元素的值，替换被删除的位置
+
+       int parent;
+
+       for(parent=index;parent*2<=maxHeap.size()-1;parent=index){
+
+           //当前节点左孩子的下标
+           index=parent*2;
+           //如果左节点不是最后一个元素，就说明一定有右子树，同时比较，左右子树，
+           if(index!=maxHeap.size()-1 && compare(maxHeap.get(index),maxHeap.get(index+1))<0){
+                //如果能够进入这个方法体，说明右孩子比左孩子大，所以index更新
+               index=index+1;
+           }
+
+           if(compare(temp,maxHeap.get(index))>0){
+               //进入这个循环体，说明当前堆顶元素，大于右孩子
+               //也就是说大于左右孩子，则不用调整，直接退出;
+               break;
+           }else{
+               // 子树上移，替换当前节点
+               maxHeap.set(parent,maxHeap.get(index));
+           }
+
+       }
+        //parent的值归位
+        maxHeap.set(parent,temp);
+        maxHeap.remove(lastIndex);//删除最后一个无用的元素
+
+      return true;
+    }
+
+
+
+
+
+}
