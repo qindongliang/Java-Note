0e43e30140b32c7872329fe2cfb8a5713868de73
Qin Dong Liang <18210507492@126.com>
2018-11-02 20:40:25 +0800

    插入排序的代码提交

--- src/main/java/algorithm/sort_algorithm/inser_sort/InsertSort.java
+++ src/main/java/algorithm/sort_algorithm/inser_sort/InsertSort.java
@@ -3,8 +3,13 @@ package algorithm.sort_algorithm.inser_sort;
 import java.util.Arrays;
 
 /****
- * 10,9,8,7,6
- *
+ * 算法思想：
+ * 遍历数组中每一个元素，每一个元素都会和前面所有的已经有序的集合元素
+ * 进行比较，从而确定每一个元素的位置，这里故意拿一个
+ * 降序好的例子，来做升序排序，比较典型的反映这种算法的思想。
+ * 平均时间复杂度：O(n^2)
+ * 平均空间复杂度：O(n)
+ * 是否稳定：是稳定排序算法
  *
  */
 
@@ -15,11 +20,13 @@ public class InsertSort {
             //取遍历的每一个元素
             int currentNumber = numbers[i];
             int j = i - 1;
-            //
+            //每一个元素，都会与前面所有的有序元素集合进行比较
+            //从而交换自己的位置
             while (j >= 0 && numbers[j] > currentNumber) {
                 numbers[j + 1] = numbers[j];
                 j--;
             }
+            //找到当前元素的位置，然后交换。
             numbers[j + 1] = currentNumber;
             System.out.println(Arrays.toString(numbers));
         }
@@ -29,9 +36,8 @@ public class InsertSort {
 
         int a[]={10,9,8,7,6};
 
-        sort(a);
+        sort(a);//排序
 
-        System.out.println(Arrays.toString(a));
 
 
     }
