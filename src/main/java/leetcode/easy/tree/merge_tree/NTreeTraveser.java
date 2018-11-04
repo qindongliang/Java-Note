29c60cd4d677e1a1140f2c26ead044eb25992393
Qin Dong Liang <18210507492@126.com>
2018-11-04 19:46:22 +0800

    添加多维树的遍历迭代获取深度的版本

--- src/main/java/leetcode/easy/tree/merge_tree/NTreeTraveser.java
+++ src/main/java/leetcode/easy/tree/merge_tree/NTreeTraveser.java
@@ -48,10 +48,37 @@ public class NTreeTraveser {
 
 //        recursion2(node1);
 
-        System.out.println((recursionMaxDeepLength(node1)));//递归读取最大深度
+//        System.out.println((recursionMaxDeepLength(node1)));//递归读取最大深度
+        bfsMaxDeepLength(node1);
 
 
 
+    }
+
+    // 559
+    private static void bfsMaxDeepLength(Node root){
+
+        if(root==null) {return;}
+
+        Queue<Node> queue=new LinkedList<>();
+        queue.offer(root);
+        int depth=0;
+
+        while(!queue.isEmpty()){
+            int size=queue.size();
+
+            for(int i=0;i<size;i++){
+                Node current=queue.poll();
+                if(current!=null&&current.children!=null){
+                    for(Node child:current.children){
+                        queue.offer(child);
+                    }
+                }
+            }
+            depth=depth+1;
+        }
+
+        System.out.println("最大深度："+depth);
     }
 
     // 559
@@ -63,9 +90,7 @@ public class NTreeTraveser {
 
         if(root.children!=null) {
             for (Node child : root.children) {
-                if (child != null) {
                     tmp = Math.max(tmp, recursionMaxDeepLength(child) + 1);
-                }
             }
         }
 
