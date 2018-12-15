43d5306c250c6282b782c2d61c1c8dbc4f192132
Qin Dong Liang <18210507492@126.com>
2018-12-15 14:18:46 +0800

    字符串反转的例子

---
+++ src/main/java/leetcode/easy/string_all/reverse_word/ReverseWord2.java
@@ -0,0 +1,63 @@
+package leetcode.easy.string_all.reverse_word;
+
+/**
+ * Created by qindongliang on 2018/12/15.
+ */
+public class ReverseWord2 {
+
+
+    public static String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(String word:s.split(" ")) {
+            char array[] = word.toCharArray();
+            for (int i = array.length - 1; i >=0; i--) {
+                sb.append(array[i]);
+            }
+            sb.append(" ");
+        }
+
+        return  sb.deleteCharAt(sb.length()-1).toString();
+    }
+
+    public static String reverseWords2(String s) {
+            char[] arr=s.toCharArray();
+        int len = s.length();
+        int start = 0;
+        for(int i = 0; i < len; i++){
+            if(arr[i] == ' '){
+                reverse(arr,start,i - 1);
+                start = i + 1;
+            }
+        }
+        reverse(arr,start,len -1);
+        return  new String(arr);
+    }
+
+    private static void reverse(char[] arr, int start, int end) {
+
+       while (start<end){
+           char temp=arr[start];
+           arr[start]=arr[end];
+           arr[end]=temp;
+           start++;
+           end--;
+       }
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        String input="Let's take LeetCode contest";
+
+        System.out.println(reverseWords(input));
+
+        System.out.println(reverseWords2(input));
+
+
+    }
+
+
+
+}
