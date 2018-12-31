0d17b590203138158628fc5a45780cf532eada39
qindongliang <18210507492@126.com>
2018-12-31 21:01:16 +0800

    assert提交

---
+++ src/main/java/basic/assert_test/DemoAssert.java
@@ -0,0 +1,35 @@
+package basic.assert_test;
+
+/**
+ * Created by qindongliang on 2018/12/31.
+ */
+public class DemoAssert {
+
+
+    public static String get(){
+        return "abccacd";
+    }
+
+
+    public static void checkName(String name){
+        assert name!=null:"name is empty";
+    }
+
+    public static void checkAge(int age){
+        assert age>0 && age<=18;
+    }
+
+
+    public static void main(String[] args) {
+
+        checkName(null);
+
+
+//        checkAge(25);
+
+
+
+
+
+    }
+}
