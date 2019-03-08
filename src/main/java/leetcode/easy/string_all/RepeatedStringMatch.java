3950b60b9101d82075a23dd901a6170172ed3368
qindongliang <qindongliang@xueleyun.com>
2019-03-08 18:31:06 +0800

    添加算法思路说明

--- src/main/java/leetcode/easy/string_all/RepeatedStringMatch.java
+++ src/main/java/leetcode/easy/string_all/RepeatedStringMatch.java
@@ -3,8 +3,11 @@ package leetcode.easy.string_all;
 /***
  * 判断重复的字符串是否包含指定的次数
  * 思路：
- * 声明一个StringBuilder，不断的追加A，然后判断sb的长度是否大于B.length
- * 如果大于就结束循环，然后判断
+ * 声明一个StringBuilder，不断的追加A，知道长度大于B.length
+ * 然后判断该子串是否在总的子串里面存在，不存在就再追加一次然后再判断。
+ * 注意这里用的lastIndexOf而不是indexOf,lastIndexOf可以从后向前搜索
+ * 如果已经知道这个子串在末尾，那么使用这个会明显提升性能。
+ *
  */
 public class RepeatedStringMatch {
 
