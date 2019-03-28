d487f5441be884c3598a21cbcc87e24af4bf04ce
qindongliang <qindongliang@xueleyun.com>
2019-03-28 21:19:04 +0800

    阿拉伯数字转中文的，保留多0的版本比如107000，读作一十万零七千，或者读作一十万七千也可以

--- src/main/java/algorithm/number_to_Chinese/NumberToChinese.java
+++ src/main/java/algorithm/number_to_Chinese/NumberToChinese.java
@@ -1,48 +1,20 @@
 package algorithm.number_to_Chinese;
 
 
-import com.google.common.collect.Lists;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 
 public class NumberToChinese {
-    private static final String[] CHINESE_NUMBERS = {"零", "一", "两", "三", "四", "五", "六", "七", "八", "九", "十"};
-    private static final ChineseUnit[] CHINESE_UNIT = {
+    private static  String[] cnNum = {"零", "一", "两", "三", "四", "五", "六", "七", "八", "九"};
+    private static  ChineseUnit[] cnUnit = {
             ChineseUnit.zero, ChineseUnit.ten, ChineseUnit.hundred, ChineseUnit.thousand, ChineseUnit.ten_thousand, ChineseUnit.billion, ChineseUnit.million, ChineseUnit.ten_million,
             ChineseUnit.hundred_mullion,ChineseUnit.ten_billon,ChineseUnit.hundred_billon,ChineseUnit.thousand_billon,ChineseUnit.ten_thousand_billon};
 
     private static class BeanUnit{
-        private ChineseUnit chineseUnit;
+        public ChineseUnit cnUnit;
+        public String cnNum;
+        public int num;
 
-       private String strNum;
 
-       private int orginNum;
-
-        public ChineseUnit getChineseUnit() {
-            return chineseUnit;
-        }
-
-        public void setChineseUnit(ChineseUnit chineseUnit) {
-            this.chineseUnit = chineseUnit;
-        }
-
-        public String getStrNum() {
-            return strNum;
-        }
-
-        public void setStrNum(String strNum) {
-            this.strNum = strNum;
-        }
-
-        public int getOrginNum() {
-            return orginNum;
-        }
-
-        public void setOrginNum(int orginNum) {
-            this.orginNum = orginNum;
-        }
     }
 
 
@@ -50,20 +22,19 @@ public class NumberToChinese {
 
         String strValue=String.valueOf(value);
         if(value<=10){
-            return CHINESE_NUMBERS[(int)value];
+            return cnNum[(int)value];
         }
-
 
         Stack<BeanUnit> stack=new Stack<>();
 
+        //从右向左入栈
         int index=0;
-
         for (int i =strValue.length()-1; i >=0 ; i--) {
             int currentNum=Integer.parseInt(strValue.charAt(i)+"");
             BeanUnit beanUnit=new BeanUnit();
-            beanUnit.setStrNum(CHINESE_NUMBERS[currentNum]);
-            beanUnit.setOrginNum(currentNum);
-            beanUnit.setChineseUnit(CHINESE_UNIT[index]);
+            beanUnit.cnNum=cnNum[currentNum];
+            beanUnit.num=currentNum;
+            beanUnit.cnUnit=cnUnit[index];
             stack.push(beanUnit);
             index++;
         }
@@ -71,36 +42,24 @@ public class NumberToChinese {
 
         StringBuilder  sb=new StringBuilder();
 
+        //从左到右出栈
         while (!stack.empty()) {
             BeanUnit bean = stack.pop();
-            if (bean.orginNum > 0) {
-                sb.append(bean.strNum);
-
-                if (bean.chineseUnit != ChineseUnit.zero) {
-                    sb.append(bean.chineseUnit.value);
-                }
-
-
-            } else if (bean.chineseUnit != ChineseUnit.zero) {
+            if (bean.num > 0) {
+                sb.append(bean.cnNum);
+               if(bean.cnUnit!=ChineseUnit.zero){
+                   sb.append(bean.cnUnit.value);
+               }
+            } else if (bean.cnUnit != ChineseUnit.zero) {//忽略个位上数字操作
                 BeanUnit nextNumber = stack.peek();
-                if(nextNumber!=null&&nextNumber.orginNum != 0) {
-//                    switch (bean.chineseUnit){
-//                        case hundred_mullion:
-//                        case ten_billon:
-//                        case hundred_billon:
-//                        case thousand_billon:
-//                        case ten_thousand_billon:
-//                            break;
-//                        default:
-//                                sb.append(bean.strNum);
-//                                break;
-//                    }
-                    sb.append(bean.strNum);
+                //按照中文大写RMB的习惯，多个0情况下只留一个就可以了
+                if(nextNumber!=null&&nextNumber.num != 0) {
+                    sb.append(bean.cnNum);
                 }
 
             }
         }
-
+        //在最终的结果里面，对亿和万只保留最小位置的单位就可以
         String filters[]=new String[]{"亿","万"};
         for (String filter:filters )
         while (sb.indexOf(filter)!=sb.lastIndexOf(filter)){
@@ -116,34 +75,17 @@ public class NumberToChinese {
 
     public static void main(String[] args) {
 
+//        System.out.println(numberToChinese(104001));
+        System.out.println(numberToChinese(111));//一百一十一
+        System.out.println(numberToChinese(194000004));//一亿九千四百万零四
+        System.out.println(numberToChinese(107000));//一十万零七千
 
-//        System.out.println(numberToChinese(104040408));//一亿零四百零四万零四百零八
-//        System.out.println(numberToChinese(100030));//一十万零三十
-//        System.out.println(numberToChinese(1030000));//一百零三万
-//        System.out.println(numberToChinese(301000010));//三亿零一百万零一十
-//        System.out.println(numberToChinese(200003));//两十万零三
-//        System.out.println(numberToChinese(100029338));//一亿零两万九千三百三十八
-//        System.out.println(numberToChinese(100209));//一十万零两百零九
-//        System.out.println(numberToChinese(33000000008L));//三百三十亿零八
-//        System.out.println(numberToChinese(8877666555L));//八十八亿七千七百六十六万六千五百五十五
-//        System.out.println(numberToChinese(302050406L));//三亿零两百零五万零四百零六
-        System.out.println(numberToChinese(3020504060L));//三十亿两千零五十万零四千零六十
-//        System.out.println(numberToChinese(30020504060L));//三百亿两千零五十万零四千零六十
-//        System.out.println(numberToChinese(504060));//五十万零四千零六十
-//        System.out.println(numberToChinese(500460));//五十万零四百六十
-//        System.out.println(numberToChinese(50460));//五万零四百六十
-//        System.out.println(numberToChinese(332114));//三十三万两千一百一十四
-//        System.out.println(numberToChinese(54123));//五万四千一百两十三
-//        System.out.println(numberToChinese(5004));//五万四千一百两十三
-//        System.out.println(numberToChinese(3050000000l));//五万四千一百两十三
-        System.out.println(numberToChinese(300034302l));//五万四千一百两十三'
-        System.out.println(numberToChinese(300004302l));//五万四千一百两十三
 
 
     }
 
 
-    static enum ChineseUnit {
+     enum ChineseUnit {
         /**
          *
          */
