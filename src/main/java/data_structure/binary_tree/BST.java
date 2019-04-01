1d614d5aaddaf9464e1179c3f36c22c8496e8e9e
qindongliang <qindongliang@xueleyun.com>
2019-04-01 18:58:48 +0800

    BST代码完善提交

--- src/main/java/data_structure/binary_tree/BST.java
+++ src/main/java/data_structure/binary_tree/BST.java
@@ -1,13 +1,16 @@
 package data_structure.binary_tree;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Stack;
 
 public class BST< T extends Comparable<T>> {
 
     public Node<T> root;
 
     public void insert(T data){
-        root=insert(root, data);
+        root=insert(root, data); //递归插入
     }
 
     public Node<T> insert(Node<T> p, T data){
@@ -25,7 +28,6 @@ public class BST< T extends Comparable<T>> {
         }else {
             p.right=insert(p.right,data);
         }
-
         return p;
     }
 
@@ -41,6 +43,89 @@ public class BST< T extends Comparable<T>> {
         inOrder(node.right);
     }
 
+    public boolean search(T data){
+      return   search(root,data);
+    }
+
+    public boolean search(Node<T> node,T data){
+        if(node==null){
+            return false;
+        }
+        if(compare(node.data,data)==0){
+            return true;
+        }else if(compare(data,node.data)<0) {
+            return search(node.left,data);
+        }else{
+            return search(node.right,data);
+        }
+
+    }
+
+
+    public void delete(T data){
+        root=delete(root,data);
+    }
+
+    public Node<T> delete(Node<T> node,T data){
+
+        if(node==null){
+            throw new RuntimeException("删除的节点不存在");
+        }else if(compare(data,node.data)<0){
+            node.left=delete(node.left,data);
+        }else if(compare(data,node.data)>0){
+            node.right=delete(node.right,data);
+        }else{//叶子节点，或者只拥有一个孩子节点的处理逻辑是一样的
+            if(node.left==null){
+                return node.right;
+            }else if(node.right==null){
+                return node.left;
+            }else{
+                //到这一步说明删除的节点拥有2个孩子节点
+                //找到剩下左子树里面最大的节点，或者找到右子树里面最小的节点，这里使用的是前者
+                //使用最大值覆盖当前要被删除的节点的值
+                node.data=retrieveData(node.left);
+                //最后删除，在剩下左子树里面刚才被替换到最大值的节点
+                node.left=delete(node.left,node.data);
+            }
+
+        }
+
+    return node;
+    }
+
+
+    private T retrieveData(Node<T> p)
+    {
+        while (p.right != null) p = p.right;
+
+        return p.data;
+    }
+
+
+    public void inOrderiterator(Node<Integer> root){
+
+        Stack<Node<Integer>> stack=new Stack<>();
+        List<Integer> list=new ArrayList<>();
+        while (root!=null||!stack.isEmpty()){
+            if(root!=null) {
+                stack.push(root);
+                root = root.left;
+            }else{
+                root=stack.pop();
+                list.add(root.data);
+                root=root.right;
+            }
+        }
+
+        System.out.println(list);
+
+
+
+    }
+
+
+
+
     public static void main(String[] args) {
 
         BST<Integer> bst=new BST<>();
@@ -50,8 +135,17 @@ public class BST< T extends Comparable<T>> {
         bst.insert(6);
         bst.insert(9);
         bst.insert(1);
+        bst.insert(11);
 
-        bst.showAll();
+//        bst.showAll();
+
+//        System.out.println(bst.search(9));
+//        bst.delete(6);
+//        System.out.println("=============================");
+        //中序递归
+//        bst.showAll();
+        //中序迭代
+        bst.inOrderiterator(bst.root);
 
     }
 
