b6ca16b1e876e116f4a103a68f7cb368b03b51d1
qindongliang <18210507492@126.com>
2019-04-03 23:43:20 +0800

    AVL树例子添加

---
+++ src/main/java/data_structure/binary_tree/AVLTree.java
@@ -0,0 +1,186 @@
+package data_structure.binary_tree;
+
+import java.util.*;
+
+/****
+ * AVL树自平衡
+ * http://btechsmartclass.com/data_structures/avl-trees.html
+ * https://gist.github.com/thmain/83d624eb2965255550c2
+ *
+ */
+public class AVLTree {
+
+    class Node{
+        int data;
+        Node left;
+        Node right;
+        int height;
+
+        public Node(int data) {
+            this.data = data;
+            height = 1;
+        }
+
+    }
+
+    public int getHeight(Node n){
+        if(n!=null){
+            return n.height;
+        }
+        return 0;
+    }
+
+    public int getBalance(Node n){
+        if(n!=null){
+            return getHeight(n.left) - getHeight(n.right);
+        }
+        return 0;
+    }
+
+
+    public Node rightRotate(Node y){
+        Node x=y.left;
+        Node T2=x.right;
+
+        //rotation
+        x.right=y;
+        y.left=T2;
+
+        x.height=Math.max(getHeight(x.left),getHeight(x.right))+1;
+        y.height=Math.max(getHeight(y.left),getHeight(y.right))+1;
+
+        return x;
+
+    }
+
+    public Node leftRotate(Node x){
+            Node y=x.right;
+            Node T2=y.left;
+
+            //Rotation
+        y.left=x;
+        x.right=T2;
+
+        //update height
+
+        x.height=Math.max(getHeight(x.left),getHeight(x.right))+1;
+        y.height=Math.max(getHeight(y.left),getHeight(y.right))+1;
+
+        return y;
+
+    }
+
+    public Node insert(Node node, int data){
+        if(node==null){
+            return new Node(data);
+        }
+
+        if(node.data>data){
+            node.left=insert(node.left,data);
+        }else if(node.data<data){
+            node.right=insert(node.right,data);
+        }else {
+            return node;// 已经存在
+        }
+
+        node.height=Math.max(getHeight(node.left),getHeight(node.right))+1;
+
+        int balDiff=getBalance(node);
+
+
+        // 左倾斜，右旋
+        if(balDiff>1&&data<node.left.data){
+            return rightRotate(node);
+        }
+
+        // 右倾斜，左旋
+
+        if(balDiff<-1&&data> node.right.data){
+            return leftRotate(node);
+        }
+
+        // 左倾斜，先左旋，再右旋
+        if(balDiff>1&&data>node.left.data){
+            node.left=leftRotate(node.left);
+            return rightRotate(node);
+        }
+
+
+        // 右倾斜，先右旋，再左旋
+        if(balDiff<-1&&data<node.right.data){
+            node.right=rightRotate(node.right);
+            return leftRotate(node);
+        }
+
+
+        return node;
+
+    }
+
+
+    public void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(" " + root.data);
+            inorder(root.right);
+        }
+    }
+
+    public List<List<Integer>> levelOrder(Node root) {
+        List result = new ArrayList();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> level = new ArrayList<Integer>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node head = queue.poll();
+                level.add(head.data);
+                if (head.left != null) {
+                    queue.offer(head.left);
+                }
+                if (head.right != null) {
+                    queue.offer(head.right);
+                }
+            }
+            result.add(level);
+        }
+        System.out.println(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Node root = null;
+        AVLTree i = new AVLTree();
+        root = i.insert(root, 1);
+        root = i.insert(root, 2);
+        root = i.insert(root, 3);
+
+        root = i.insert(root, 4);
+        root = i.insert(root, 5);
+        i.levelOrder(root);
+//        root = i.insert(root, 6);
+//        root = i.insert(root, 9);
+//        root = i.insert(root, 14);
+//        root = i.insert(root, 11);
+//        root = i.insert(root, 10);
+
+
+//        i.inorder(root);
+
+
+
+    }
+
+
+}
+
+
+
