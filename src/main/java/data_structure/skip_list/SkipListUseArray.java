c56397169fb11c8d9cfa7e485cca7a58b9dd63fc
qindongliang <qindongliang@xueleyun.com>
2019-04-22 19:00:50 +0800

    增加相关注释

--- src/main/java/data_structure/skip_list/SkipListUseArray.java
+++ src/main/java/data_structure/skip_list/SkipListUseArray.java
@@ -8,39 +8,102 @@ import java.util.Random;
 public class SkipListUseArray {
 
 
+    public class Node{
+
+        private int value;
+        private Node next[];//指向下一个节点
+        private int level;//跨越几层
+
+        public Node(int value, int level ){
+            this.value=value;
+            this.level=level;
+            this.next=new Node[level];
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb=new StringBuilder();
+            sb.append("value:").append(value);
+            sb.append(" levels:").append(level);
+            return sb.toString();
+        }
+    }
+
+
+
     private static final int MAX_LEVEL=16;
 
     private int levelCount=1;
 
-    private Node head=new Node();//声明头部节点
+    private Node head=new Node(-1,MAX_LEVEL);//声明头部节点，充当辅助
 
     private Random random=new Random();
 
+    int size=0;
+
+
 
     /***
-     * 通过奇偶数的方式来决定随机节点的位置
+     * 通过奇偶数的方式来生成随机生成层级
      * @return
      */
-    private int randomLevel(){
+    private int getLevel(){
         int level=1;
-        for (int i=1;i<MAX_LEVEL;i++){
-            if(random.nextInt()%2==1){
+        while (true){
+            int r=random.nextInt();
+            if(r%2==0){
                 level++;
+            }else {
+                break;
             }
         }
+        System.out.println("本次生成的level："+level);
         return level;
     }
+
+    public void insert(int value){
+
+        int level=getLevel();
+        //新生成的节点
+        Node newData=new Node(value, level);
+        //update用于记录要插入的节点的前驱
+        Node update[]=new Node[level];
+
+        Node temp=head;
+
+        for (int i = level-1; i >=0 ; i--) {
+            while (temp.next[i]!=null&&temp.next[i].value <value){
+                temp=temp.next[i];
+            }
+            update[i]=temp;
+        }
+
+        //把新增的节点，加到前驱节点的后面即可
+        for (int i = 0; i < level; i++) {
+            newData.next[i]=update[i].next[i];
+            update[i].next[i]=newData;
+        }
+
+        if(levelCount<level){
+            levelCount=level;
+        }
+
+        size++;
+    }
+
 
 
     public Node find(int value){
         Node p=head;
+        //从高到底按层级搜索
         for (int i = levelCount-1; i >=0; i--) {
-            while (p.forwards[i]!=null&&p.forwards[i].data<value){
-                p=p.forwards[i];
+            while (p.next[i]!=null&&p.next[i].value <value){
+                p=p.next[i];
             }
         }
-        if(p.forwards[0]!=null&&p.forwards[0].data==value){
-            return p.forwards[0];
+        //最后找到的节点，判断下是否等于输入的然后就返回
+        if(p.next[0]!=null&&p.next[0].value ==value){
+            return p.next[0];
         }else{
             return null;
         }
@@ -49,65 +112,34 @@ public class SkipListUseArray {
     public void delete(int value){
         Node[] update=new Node[levelCount];
         Node p=head;
+        //找到所有查询value的前驱节点
         for (int i = levelCount-1; i >=0 ; i--) {
-            while (p.forwards[i]!=null&&p.forwards[i].data<value){
-                p=p.forwards[i];
+            while (p.next[i]!=null&&p.next[i].value <value){
+                p=p.next[i];
             }
             update[i]=p;
         }
 
-        if(p.forwards[0]!=null&&p.forwards[0].data==value){
+        if(p.next[0]!=null&&p.next[0].value ==value){
+            size--;
             for (int i = levelCount-1; i >=0 ; i--) {
-                if(update[i].forwards[i]!=null&&update[i].forwards[i].data==value){
-                update[i].forwards[i]=update[i].forwards[i].forwards[i];
+                if(update[i].next[i]!=null&&update[i].next[i].value ==value){
+                 //设置当前节点的后缀的等于删除的后缀即可
+                update[i].next[i]=update[i].next[i].next[i];
                 }
             }
         }
     }
 
 
-    public void insert(int value){
 
-        int level=randomLevel();
-        Node newData=new Node();
-
-        newData.data=value;
-        newData.maxLevel=level;
-
-        Node update[]=new Node[level];
-
-        for (int i = 0; i <level ; i++) {
-            update[i]=head;
-        }
-
-        Node p=head;
-
-        for (int i = level-1; i >=0 ; i--) {
-            while (p.forwards[i]!=null&&p.forwards[i].data<value){
-                p=p.forwards[i];
-            }
-            update[i]=p;
-        }
-
-
-        for (int i = 0; i < level; i++) {
-            newData.forwards[i]=update[i].forwards[i];
-            update[i].forwards[i]=newData;
-        }
-
-        if(levelCount<level){
-            levelCount=level;
-        }
-
-
-    }
 
 
     public void printAll(){
         Node p=head;
-        while (p.forwards[0]!=null){
-            System.out.println(p.forwards[0]+" ");
-            p=p.forwards[0];
+        while (p.next[0]!=null){
+            System.out.println(p.next[0]+" ");
+            p=p.next[0];
         }
     }
 
@@ -115,12 +147,16 @@ public class SkipListUseArray {
     public static void main(String[] args) {
 
         SkipListUseArray list=new SkipListUseArray();
-        list.insert(4);
-        list.insert(41);
-        list.insert(23);
+
+        list.insert(10);
+        list.insert(12);
+        list.insert(14);
+        list.insert(20);
         list.insert(8);
-        list.insert(2);
-        list.insert(6);
+        list.insert(7);
+        list.insert(3);
+        list.insert(1);
+
 
 
 //        System.out.println(list);
@@ -134,21 +170,6 @@ public class SkipListUseArray {
 
 
 
-    public class Node{
-
-        private int data=-1;
-        private Node forwards[]=new Node[MAX_LEVEL];
-
-        private int maxLevel=0;
-
-        @Override
-        public String toString() {
-            StringBuilder sb=new StringBuilder();
-            sb.append("data:").append(data);
-            sb.append(" levels:").append(maxLevel);
-            return sb.toString();
-        }
-    }
 
 
 
