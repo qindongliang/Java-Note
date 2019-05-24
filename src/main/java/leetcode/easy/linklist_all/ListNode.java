1c895dbf491c55656ce1cbfe1e43f8ac5d37db20
qindongliang <qindongliang@xueleyun.com>
2019-05-24 10:36:56 +0800

    separate ListNode from MyLinkList class

---
+++ src/main/java/leetcode/easy/linklist_all/ListNode.java
@@ -0,0 +1,12 @@
+package leetcode.easy.linklist_all;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+
+    @Override
+    public String toString() {
+        return val+"";
+    }
+}
