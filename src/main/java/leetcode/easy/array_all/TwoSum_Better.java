bf1eb8688b59745c6ed1622aaa684c2421b8ccd9
qindongliang <18210507492@126.com>
2019-05-29 22:54:18 +0800

    addition comment

--- src/main/java/leetcode/easy/array_all/TwoSum_Better.java
+++ src/main/java/leetcode/easy/array_all/TwoSum_Better.java
@@ -7,6 +7,15 @@ import java.util.HashMap;
 
 /**
  * Created by qindongliang on 2018/7/2.
+ *
+ *
+ * 思路如下：
+ *
+ * 声明一个Map，然后遍历数组，取目标值减去每一个迭代到的值，然后取map里面
+ * 查询，如果查到就代表找到了。直接返回。
+ *
+ * 如果没有查到，就把遍历的数字和下标放进Map里面继续迭代，直到找到目标值。
+ *
  */
 public class TwoSum_Better {
 
