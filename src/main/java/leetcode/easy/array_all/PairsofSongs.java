b2a176db3a97b18b1592242aa919b4b91c6f199f
qindongliang <qindongliang@xueleyun.com>
2019-06-04 11:09:50 +0800

    add comment to pairs of songs

--- src/main/java/leetcode/easy/array_all/PairsofSongs.java
+++ src/main/java/leetcode/easy/array_all/PairsofSongs.java
@@ -36,21 +36,37 @@ public class PairsofSongs {
         if(time==null|time.length==0){
             return ans;
         }
-        //map存储计数信息
         Map<Integer,Integer> map=new HashMap<>();
-        //顺序遍历数组
         for (int i = 0; i < time.length; i++) {
             int rem=time[i]%60;//得到这个数的余数 [0..59]
-            // 60 - [0..59] = [60..1]
-
-            ans+=map.getOrDefault((60-rem)%60,0);
-            //存储rem
+            int v=60-rem;//1..60，如果v==60说明time[i]必定被整除，rem=0
+            ans+=map.getOrDefault(v==60?0:v,0);//当v=60的时候，需要归一成0，否则rem的取值，会漏掉0的处理
             map.put(rem,map.getOrDefault(rem,0)+1);
         }
         return ans;
     }
 
 
+    /****
+     * 两个数相加%60=0，那么两个数分别%60的和再%60必定也能被整除。
+     * @param time
+     * @return
+     */
+    public static int easyRead(int[] time) {
+        int []count=new int[60];
+        int songCnt=0;
+        for (int num:time){
+            //得到一个数的余数
+            int remainder=num%60;
+            //这个地方如果num%60=0，那么v=60，说明是整除了，但存存放数组会越界
+            int v=60-remainder;//这个范围是1..60,因为我们要用60，所以在遇到60的时候，存到数组index=0的地方，避免越界
+            //如果count[v]余数如果有值，就等于两个余数的和=60，那么这个数必定符合要求
+            songCnt=songCnt+count[v==60?0:v];
+            //存储余数
+            count[remainder]=count[remainder]+1;
+        }
+        return songCnt;
+    }
 
 
     public static int numPairsDivisibleBy602(int[] time) {
@@ -58,7 +74,7 @@ public class PairsofSongs {
         int songCnt=0;
         for (int num:time){
             //将结果映射到1..60这个数据范围上
-            songCnt=songCnt+count[(60-num%60)%60];
+            songCnt=songCnt+count[(60-num%60)%60];//这里再次%60实际上是把边界重新分为0..59，避免越界
             //如果发现重复，则加1
             count[num%60]=count[num%60]+1;
         }
