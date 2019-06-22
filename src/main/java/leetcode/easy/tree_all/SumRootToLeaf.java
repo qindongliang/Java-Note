dd02f81b48d6e7dfb28d4a90dc873c0fb9ff20d9
qindongliang <18210507492@126.com>
2019-06-23 00:41:35 +0800

    sum root to leaf

--- src/main/java/leetcode/easy/tree_all/SumRootToLeaf.java
+++ src/main/java/leetcode/easy/tree_all/SumRootToLeaf.java
@@ -2,31 +2,87 @@ package leetcode.easy.tree_all;
 
 import java.util.Stack;
 
+/****
+ *
+ * 链接：https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/
+ *
+ * 描述：给定一个二叉树，让遍历出所有的路径，这个路径是个二进制的数字，然后转成10进制后求和
+ *
+ * 思路：
+ *
+ * （1）使用递归
+ *
+ * （2）使用迭代，迭代版本稍微复杂点，因为需要封装一个数据结构
+ *
+ *
+ */
 public class SumRootToLeaf {
+
+    static class Pair<U,V>{
+        public  U first;
+        public  V second;
+
+        public Pair(){
+
+        }
+
+        public Pair(U first,V second){
+            this.first=first;
+            this.second=second;
+        }
+
+
+    }
+
+    public static boolean isLeaf(TreeNode node){
+        return node.left==null&&node.right==null;
+    }
 
 
     public static int sumRootToLeaf(TreeNode root) {
         if(root==null) return 0;
+        int sum=0;
+        Stack<Pair<TreeNode,String>> stack=new Stack<>();
 
-        Stack<TreeNode> stack=new Stack<>();
-        stack.push(root);
-        StringBuilder sb=new StringBuilder();
+        stack.push(new Pair<TreeNode,String>(root,""));
+
         while (!stack.isEmpty()){
+            Pair<TreeNode,String> pair=stack.pop();
+            TreeNode curr=pair.first;
+            String sb=pair.second;
+            sb=sb+curr.val;
 
-           TreeNode temp= stack.pop();
+            if(isLeaf(curr)){ //leaf node
+                sum+=Integer.parseInt(sb,2);
 
-           if(temp.right!=null) {
-               stack.push(temp.right);
-           }
-            if(temp.left!=null) {
-                stack.push(temp.left);
             }
-
+            if(curr.right!=null) stack.push(new Pair<>(curr.right,sb));
+            if(curr.left!=null) stack.push(new Pair<>(curr.left,sb));
 
 
         }
 
-      return 0;
+      return sum;
+    }
+
+
+    public static int sumRootToLeafRecursion(TreeNode root ,StringBuilder sb) {
+        if(root==null) return 0;
+        sb.append(root.val);
+        if(isLeaf(root)){
+            System.out.println(sb.toString());
+        }
+        sumRootToLeafRecursion(root.left,sb);
+        sumRootToLeafRecursion(root.right,sb);
+        sb.deleteCharAt(sb.length()-1);
+        return 0;
+    }
+
+
+    public static int sumRootToLeafRecursion(TreeNode root) {
+        sumRootToLeafRecursion(root,new StringBuilder(""));
+
+        return 0;
     }
 
 
@@ -48,8 +104,9 @@ public class SumRootToLeaf {
         s4.left=s5;
         s4.right=s6;
 
-        sumRootToLeaf(root);
+        System.out.println(sumRootToLeaf(root));
 
+//        sumRootToLeafRecursion(root);
 
 
     }
