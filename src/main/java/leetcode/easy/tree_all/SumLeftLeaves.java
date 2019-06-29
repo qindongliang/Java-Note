05d802436fc64de0a64cad7dff9d42adaf4052a7
qindongliang <qindongliang@xueleyun.com>
2019-06-29 22:41:57 +0800

    Sum of Left Leaves

--- src/main/java/leetcode/easy/tree_all/SumLeftLeaves.java
+++ src/main/java/leetcode/easy/tree_all/SumLeftLeaves.java
@@ -3,10 +3,30 @@ package leetcode.easy.tree_all;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/****
+ *
+ * https://leetcode.com/problems/sum-of-left-leaves/
+ *
+ * 题目描述：给定一个二叉树，让求出所有该树左叶子节点的和
+ *
+ * 思路：其实该题比较简单，直接按照题意求解即可
+ *
+ *
+ */
 public class SumLeftLeaves {
     public int sumOfLeftLeaves(TreeNode root) {
         int sum = 0;
+        if (root == null) return 0;
 
+        if (root.left != null) {
+            if (root.left.left == null && root.left.right == null) {
+                sum += root.left.val;
+            } else {
+                sum += sumOfLeftLeaves(root.left);
+            }
+        }
+
+        sum += sumOfLeftLeaves(root.right);
 
         return sum;
     }
