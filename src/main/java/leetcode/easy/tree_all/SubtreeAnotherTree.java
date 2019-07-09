24b3f512ee58c82ac3513ee49fb9ffb2f122e5ea
qindongliang <qindongliang@xueleyun.com>
2019-07-09 22:56:17 +0800

    Subtree of Another Tree

---
+++ src/main/java/leetcode/easy/tree_all/SubtreeAnotherTree.java
@@ -0,0 +1,57 @@
+package leetcode.easy.tree_all;
+
+import java.util.Stack;
+
+public class SubtreeAnotherTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+        if (isSame(s, t)) return true;
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    private boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+
+        if (s.val != t.val) return false;
+
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
+
+    public boolean isSubtree2(TreeNode s, TreeNode t) {
+        String sAppend = generatePreOrderString(s);
+        String tAppend = generatePreOrderString(t);
+        System.out.println(sAppend);
+        System.out.println(tAppend);
+        return sAppend.contains(tAppend);
+
+    }
+
+    public String generatePreOrderString(TreeNode s) {
+        StringBuilder sb = new StringBuilder();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(s);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node == null) {
+                sb.append(",#");
+            } else {
+                sb.append(",").append(node.val);
+            }
+
+            if (node != null) {
+                stack.push(node.right);
+                stack.push(node.left);
+            }
+        }
+        return sb.toString();
+    }
+
+}
