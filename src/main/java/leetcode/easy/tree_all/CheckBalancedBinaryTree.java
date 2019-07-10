a7684ab16d68c1df59b42afa6dae06fb9e03f81c
qindongliang <qindongliang@xueleyun.com>
2019-07-10 22:51:19 +0800

    Balanced Binary Tree

--- src/main/java/leetcode/easy/tree_all/CheckBalancedBinaryTree.java
+++ src/main/java/leetcode/easy/tree_all/CheckBalancedBinaryTree.java
@@ -25,11 +25,15 @@ public class CheckBalancedBinaryTree {
         if (node == null) {
             return 0;
         }
+        //求左子树高度
         int left = backTrack(node.left);
+        //求右子树的高度
         int right = backTrack(node.right);
+        //如果他们的绝对值大于1，就说明不均衡
         if (Math.abs(left - right) > 1) {
             isBalanced = false;
         }
+        //如果符合均衡，就返回这个子树最大的高度，继续回溯判断
         return Math.max(left, right) + 1;
     }
 
