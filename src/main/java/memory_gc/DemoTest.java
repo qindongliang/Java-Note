4a1a52bf1bcde1a78a9fe141d7da8f8ac3ab2851
qindongliang <qindongliang@xueleyun.com>
2019-07-19 14:20:26 +0800

    java 数据类型分布测试

---
+++ src/main/java/memory_gc/DemoTest.java
@@ -0,0 +1,18 @@
+package memory_gc;
+
+/***
+ *
+ * 展示堆栈数据的分布
+ *
+ */
+public class DemoTest {
+
+    int y;// 分布在堆上
+
+    public static void main(String[] args) {
+
+        int x=1; //分配在栈上
+    }
+
+
+}
