06b751e693eaab4c4152cd1735b3926be86c149c
qindongliang <qindongliang@xueleyun.com>
2019-07-19 16:17:16 +0800

    java 数据类型分布测试例子

--- src/main/java/memory_gc/DemoTest.java
+++ src/main/java/memory_gc/DemoTest.java
@@ -13,6 +13,8 @@ public class DemoTest {
 
         int x=1; //分配在栈上
         String name=new String("cat");//数据在堆上，name变量的指针在栈上
+        String address="北京";//数据在常量池，属于堆空间，指针在栈
+        Integer price=4;//包装类型同样是引用类型，编译时会自动装拆相，所以数据在堆上，指针在栈
     }
 
 
