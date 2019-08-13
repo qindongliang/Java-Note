28003d9e173f3145560535ae591fe07931ec21ff
qindongliang <qindongliang@xueleyun.com>
2019-08-13 21:40:28 +0800

    Count Primes

---
+++ src/main/java/leetcode/easy/hash_table_all/CountPrimes.java
@@ -0,0 +1,13 @@
+package leetcode.easy.hash_table_all;
+
+public class CountPrimes {
+
+    public int countPrimes(int n) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
