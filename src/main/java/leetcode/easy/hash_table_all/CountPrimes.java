aefb83a5bc75e2845e42ec0a8f2240b2e1ffd76b
qindongliang <qindongliang@xueleyun.com>
2019-08-14 21:44:52 +0800

    Count Primes

--- src/main/java/leetcode/easy/hash_table_all/CountPrimes.java
+++ src/main/java/leetcode/easy/hash_table_all/CountPrimes.java
@@ -1,10 +1,29 @@
 package leetcode.easy.hash_table_all;
 
+/****
+ *
+ * https://leetcode.com/problems/count-primes/
+ *
+ * 给定一个数字n，求在n之内这个范围内素数的个数
+ *
+ */
 public class CountPrimes {
 
     public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            //统计素数的的个数
+            if (notPrime[i] == false) {
+                count++;
+                //把合数置为true
+                for (int j = 2; i*j < n; j++) {
+                    notPrime[i*j] = true;
+                }
+            }
+        }
 
-        return 0;
+        return count;
     }
 
     public static void main(String[] args) {
